8d93daf721d8e82cbfaeb2ccdce9faf082f0393e
Jesper Stemann Andersen <jsa@ihp.dk>
2021-05-03 10:32:38 +0200

    Added SerializingChannel that communicates serialized values
    Values are serialized using an AbstractTypeSerializer.
    Serialized messages are transpored across an AbstractChannel{Vector{UInt8}}.

--- src/SerializingChannels.jl
+++ src/SerializingChannels.jl
@@ -1,3 +1,52 @@
 module SerializingChannels
 
+using TypeSerializers
+
+export SerializingChannel, put!, take!, isready, fetch, close, isopen, bind, iterate
+
+struct SerializingChannel{T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    channel::TChannel
 end
+
+function Base.put!(channel::SerializingChannel{T, TChannel, TSerializer}, value::T) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    stream = serialize(TSerializer, value)
+    data = take!(stream)
+    put!(channel.channel, data)
+end
+
+function Base.take!(channel::SerializingChannel{T, TChannel, TSerializer})::T where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    message = take!(channel.channel)
+    stream = IOBuffer(message)
+    return deserialize(TSerializer, stream)
+end
+
+function Base.isready(channel::SerializingChannel{T, TChannel, TSerializer}) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    return isready(channel.channel)
+end
+
+function Base.fetch(channel::SerializingChannel{T, TChannel, TSerializer}) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    message = fetch(channel.channel)
+    stream = IOBuffer(message)
+    return deserialize(TSerializer, stream)
+end
+
+function Base.close(channel::SerializingChannel{T, TChannel, TSerializer}) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    return close(channel.channel)
+end
+
+function Base.isopen(channel::SerializingChannel{T, TChannel, TSerializer}) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    return isopen(channel.channel)
+end
+
+function Base.bind(channel::SerializingChannel{T, TChannel, TSerializer}, task::Task) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    return bind(channel.channel, task)
+end
+
+function Base.iterate(channel::SerializingChannel{T, TChannel, TSerializer}, state::Any=nothing) where {T, TChannel <: AbstractChannel{Vector{UInt8}}, TSerializer <: AbstractTypeSerializer{T}}
+    if (isopen(channel))
+        return (take!(channel), nothing)
+    end
+    return nothing
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
+using SerializingChannels
 using Test
+using TypeSerializers
 
 @testset "SerializingChannels" begin
+    integer_value = 42
+    c = Channel{Vector{UInt8}}(1)
+    integer_channel = SerializingChannel{Int, Channel{Vector{UInt8}}, SerializationTypeSerializer{Int}}(c)
+    @test !isready(integer_channel)
+    @test !isready(c)
+    put!(integer_channel, integer_value)
+    @test isready(integer_channel)
+    @test isready(c)
+    @test take!(integer_channel) == integer_value
 end
